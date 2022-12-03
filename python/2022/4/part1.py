bf7d95e85e1d8c48370b9363572ca74a7b43107e
Liam Mullens <liammullens42@gmail.com>
2022-12-04 10:33:01 +1100

    Restructure directories, add day 4 template

---
+++ python/2022/4/part1.py
@@ -0,0 +1,9 @@
+from aocd import get_data, submit
+
+data = get_data(day=4, year=2022)
+data = """"""
+
+            
+
+print(total)
+submit(total, part="a", day=4, year=2022)
---
+++ python/2022/4/part2.py
@@ -0,0 +1,8 @@
+from aocd import get_data, submit
+
+data = get_data(day=4, year=2022)
+data = """"""
+            
+
+print(total)
+submit(total, part="b", day=4, year=2022)
