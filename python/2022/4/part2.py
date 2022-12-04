9dab281d9f5e1ed881bb8c20ab017f91005a74fa
mail-liam <30469495+mail-liam@users.noreply.github.com>
2022-12-05 00:28:52 +1100

    Day 4 Python

--- python/2022/4/part2.py
+++ python/2022/4/part2.py
@@ -1,8 +1,18 @@
 from aocd import get_data, submit
 
 data = get_data(day=4, year=2022)
-data = """"""
-            
+
+def check_partial_overlap(num_range: str) -> bool:
+    first, second = num_range.split(",")
+    l1, l2 = [int(n) for n in first.split("-")]
+    r1, r2 = [int(n) for n in second.split("-")]
+
+    return l2 >= r1 and r2 >= l1
+
+total = 0
+for pair in data.split("\n"):
+    if check_partial_overlap(pair):
+        total += 1
 
 print(total)
 submit(total, part="b", day=4, year=2022)
