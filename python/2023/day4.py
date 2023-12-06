c6c48316784153194358dc6b857a582d461216a8
mail-liam <30469495+mail-liam@users.noreply.github.com>
2023-12-06 16:34:08 +1100

    Have other days use EXAMPLE_DATA. Found extra parse_numbers

--- python/2023/day4.py
+++ python/2023/day4.py
@@ -1,23 +1,19 @@
 import math
 import re
 
-def parse_numbers(numbers):
-    res = []
-    for n in numbers.split(" "):
-        try:
-            res.append(int(n))
-        except ValueError:
-            continue
+from common.parsing import parse_numbers
 
-    return res
+
+EXAMPLE_DATA = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
 
 def part1(data):
-#     data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+    # data = EXAMPLE_DATA
     
     class Card:
         def __init__(self, winning, numbers):
@@ -45,12 +41,7 @@ def part1(data):
 
 
 def part2(data):
-    # data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-# Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
-# Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
-# Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
-# Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
-# Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+    # data = EXAMPLE_DATA
 
     class Card:
         def __init__(self, id, winning, numbers):
@@ -69,7 +60,7 @@ def part2(data):
     CARDS = []
     for line in data.splitlines():
         card_no, cards = line.split(": ")
-        card_number = int(re.search(f"\d+", card_no)[0])
+        card_number = int(re.search(r"\d+", card_no)[0])
         
         winning, entries = cards.split(" | ")
         winning_nums, entry_nums = parse_numbers(winning), parse_numbers(entries)
--- python/2023/day5.py
+++ python/2023/day5.py
@@ -3,6 +3,41 @@ import itertools
 from common.parsing import parse_numbers
 
 
+EXAMPLE_DATA = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
 def get_transform_data_p1(data):
     transforms = []
     current_transform = []
@@ -58,39 +93,7 @@ def process_seed(seed_no, data):
 
 
 def part1(data):
-#     data = """seeds: 79 14 55 13
-
-# seed-to-soil map:
-# 50 98 2
-# 52 50 48
-
-# soil-to-fertilizer map:
-# 0 15 37
-# 37 52 2
-# 39 0 15
-
-# fertilizer-to-water map:
-# 49 53 8
-# 0 11 42
-# 42 0 7
-# 57 7 4
-
-# water-to-light map:
-# 88 18 7
-# 18 25 70
-
-# light-to-temperature map:
-# 45 77 23
-# 81 45 19
-# 68 64 13
-
-# temperature-to-humidity map:
-# 0 69 1
-# 1 0 69
-
-# humidity-to-location map:
-# 60 56 37
-# 56 93 4"""
+    # data = EXAMPLE_DATA
 
     split_data = data.splitlines()
     seed_data, map_data = split_data[0], split_data[2:]
@@ -105,39 +108,7 @@ def part1(data):
 
 
 def part2(data):
-#     data = """seeds: 79 14 55 13
-
-# seed-to-soil map:
-# 50 98 2
-# 52 50 48
-
-# soil-to-fertilizer map:
-# 0 15 37
-# 37 52 2
-# 39 0 15
-
-# fertilizer-to-water map:
-# 49 53 8
-# 0 11 42
-# 42 0 7
-# 57 7 4
-
-# water-to-light map:
-# 88 18 7
-# 18 25 70
-
-# light-to-temperature map:
-# 45 77 23
-# 81 45 19
-# 68 64 13
-
-# temperature-to-humidity map:
-# 0 69 1
-# 1 0 69
-
-# humidity-to-location map:
-# 60 56 37
-# 56 93 4"""
+    # data = EXAMPLE_DATA
 
     split_data = data.splitlines()
     seed_data, map_data = split_data[0], split_data[2:]
