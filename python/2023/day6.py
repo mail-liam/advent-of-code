ee9f7bc71f6deae2747e06b7fe130cf3d7e25f14
mail-liam <30469495+mail-liam@users.noreply.github.com>
2023-12-06 16:17:17 +1100

    Python 2023 Day 6

--- python/2023/day6.py
+++ python/2023/day6.py
@@ -1,6 +1,57 @@
+import operator
+import re
+from functools import reduce
+
+EXAMPLE_DATA = """Time:      7  15   30
+Distance:  9  40  200"""
+
 def part1(data):
-    pass
+    # data = EXAMPLE_DATA
+
+    times, records = data.splitlines()
+
+    race_times = (int(time) for time in re.findall(r"\d+", times))
+    race_records = (int(record) for record in re.findall(r"\d+", records))
+    races = zip(race_times, race_records)
+
+    win_options = []
+
+    for time, record in races:
+        has_won = False
+        possible_wins = 0
+
+        for hold_time in range(time):
+            remaining_time = time - hold_time
+            distance = hold_time * remaining_time
+
+            if distance > record:
+                has_won = True
+                possible_wins += 1
+            elif has_won:
+                win_options.append(possible_wins)
+                break
+
+    print(win_options)
+    return reduce(operator.mul, win_options)
 
 
 def part2(data):
-    pass
+    # data = EXAMPLE_DATA
+
+    times, records = data.splitlines()
+
+    time = int("".join(time for time in re.findall(r"\d+", times)))
+    record = int("".join(record for record in re.findall(r"\d+", records)))
+
+    has_won = False
+    possible_wins = 0
+
+    for hold_time in range(time):
+        remaining_time = time - hold_time
+        distance = hold_time * remaining_time
+
+        if distance > record:
+            has_won = True
+            possible_wins += 1
+        elif has_won:
+            return possible_wins
