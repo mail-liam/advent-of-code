c276096db3c2fdf4421d1c660e5c978f6b77d661
mail-liam <30469495+mail-liam@users.noreply.github.com>
2023-12-10 11:12:48 +1100

    Rust 2022 Day 1 part 2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,5 @@
 use aoc_client::{AocClient, AocResult};
-use crate::year2022::day1::part1;
+use crate::year2022::day1::part2;
 use std::path::Path;
 
 pub mod year2022;
@@ -13,7 +13,7 @@ fn main() -> AocResult<()> {
 
     let data: String = client.get_input()?;
 
-    let result = part1(data);
+    let result = part2(data);
     println!("{}", result);
 
     Ok(())
--- rust/src/year2022/day1.rs
+++ rust/src/year2022/day1.rs
@@ -1,19 +1,4 @@
-pub fn part1(data: String) -> i32 {
-//     let data = "1000
-// 2000
-// 3000
-
-// 4000
-
-// 5000
-// 6000
-
-// 7000
-// 8000
-// 9000
-
-// 10000";
-
+fn get_totals(data: String) -> Vec<i32> {
     let mut totals: Vec<i32> = Vec::new();
     let mut current = 0;
     for line in data.lines() {
@@ -26,5 +11,51 @@ pub fn part1(data: String) -> i32 {
     }
     totals.push(current);
 
+    totals
+}
+
+pub fn part1(data: String) -> i32 {
+    let totals = get_totals(data);
+
     *totals.iter().max().unwrap()
+}
+
+
+pub fn part2(data: String) -> i32 {
+    let mut totals = get_totals(data);
+    totals.sort();
+    totals.reverse();
+
+    totals[0] + totals[1] + totals[2]
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE_DATA: &str = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(String::from(EXAMPLE_DATA)), 24000);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(String::from(EXAMPLE_DATA)), 45000);
+    }
 }
