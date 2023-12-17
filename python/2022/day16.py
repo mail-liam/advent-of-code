26f6d54e46c798a7c19c9d2f223bbe93d34aa140
mail-liam <30469495+mail-liam@users.noreply.github.com>
2023-12-17 22:33:49 +1100

    Convert Day 16

--- python/2022/day16.py
+++ python/2022/day16.py
@@ -1,22 +1,19 @@
 import re
 from collections import deque
 
-from aocd import get_data, submit
 
-data = get_data(day=16, year=2022)
-# data = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-# Valve BB has flow rate=13; tunnels lead to valves CC, AA
-# Valve CC has flow rate=2; tunnels lead to valves DD, BB
-# Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
-# Valve EE has flow rate=3; tunnels lead to valves FF, DD
-# Valve FF has flow rate=0; tunnels lead to valves EE, GG
-# Valve GG has flow rate=0; tunnels lead to valves FF, HH
-# Valve HH has flow rate=22; tunnel leads to valve GG
-# Valve II has flow rate=0; tunnels lead to valves AA, JJ
-# Valve JJ has flow rate=21; tunnel leads to valve II"""
+EXAMPLE_DATA = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II"""
 
-INPUT_RE = re.compile("Valve (\w\w) has flow rate=(\d*); tunnel[s]? lead[s]? to valve[s]? (.*)")
-VALVE_MAP = {}
+INPUT_RE = re.compile(r"Valve (\w\w) has flow rate=(\d*); tunnel[s]? lead[s]? to valve[s]? (.*)")
 
 class Valve:
     def __init__(self, name, flow_rate, connections):
@@ -29,20 +26,15 @@ class Valve:
 
     def __lt__(self, other):
         return self.flow_rate < other.flow_rate
-
-
-for line in data.splitlines():
-    name, flow_rate, connections = INPUT_RE.match(line).groups()
-    VALVE_MAP[name] = Valve(name=name, flow_rate=int(flow_rate), connections=connections.split(", "))
-
+    
 
 def score_valve(valve, distance, timer):
     score = valve.flow_rate * (timer - distance - 1)  # One to open
-    # print(f"Score for {valve} at distance {distance}: {score}")
+
     return max(score, 0)
 
 
-def choose_valve(current_location, current_used, current_total, timer):
+def choose_valve(valve_map, current_location, current_used, current_total, timer):
     totals = []
     seen = set()
     seen.add(current_location)
@@ -54,7 +46,7 @@ def choose_valve(current_location, current_used, current_total, timer):
         current_name, distance = to_visit.popleft()
         if distance > timer:
             break
-        current_valve = VALVE_MAP[current_name]
+        current_valve = valve_map[current_name]
 
         for name in current_valve.connections:
             if name in seen:
@@ -83,6 +75,7 @@ def choose_valve(current_location, current_used, current_total, timer):
         # timer={new_timer}""")
 
         total = choose_valve(
+            valve_map=valve_map,
             current_location=new_location,
             current_used=new_used,
             current_total=new_total,
@@ -94,19 +87,36 @@ def choose_valve(current_location, current_used, current_total, timer):
     else:
         highest_total = sorted(totals)[-1]
     highest_total[1].append(current_location)
-    # print(f"Returning total: {highest_total}")
+
     return highest_total
 
 
-total, path = choose_valve(current_location="AA", current_used={"BT"}, current_total=0, timer=26)
+def part1(data):
+    # data = EXAMPLE_DATA
 
-print(total)
-print(list(reversed(path)))
+    VALVE_MAP = {}
+    for line in data.splitlines():
+        name, flow_rate, connections = INPUT_RE.match(line).groups()
+        VALVE_MAP[name] = Valve(name=name, flow_rate=int(flow_rate), connections=connections.split(", "))
 
-total2, path2 = choose_valve(current_location="AA", current_used=set(path), current_total=0, timer=26)
+    total, path = choose_valve(valve_map=VALVE_MAP, current_location="AA", current_used=set(), current_total=0, timer=30)
+    print(list(reversed(path)))
+    return total
 
-print(total2)
-print(list(reversed(path2)))
 
-print(f"Combined total: {total + total2}")
-# submit(total, part="a", day=16, year=2022)
+def part2(data):
+    VALVE_MAP = {}
+    for line in data.splitlines():
+        name, flow_rate, connections = INPUT_RE.match(line).groups()
+        VALVE_MAP[name] = Valve(name=name, flow_rate=int(flow_rate), connections=connections.split(", "))
+
+    total, path = choose_valve(valve_map=VALVE_MAP, current_location="AA", current_used={"BT"}, current_total=0, timer=26)
+
+    print(total)
+    print(list(reversed(path)))
+
+    total2, path2 = choose_valve(valve_map=VALVE_MAP, current_location="AA", current_used=set(path), current_total=0, timer=26)
+    print(total2)
+    print(list(reversed(path2)))
+
+    return total + total2
