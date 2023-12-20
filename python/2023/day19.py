a585566fe7e3d1bf0798cd9c167502c88f4fb793
mail-liam <30469495+mail-liam@users.noreply.github.com>
2023-12-21 09:37:04 +1100

    Python 2023 Day 19 Part 2

--- python/2023/day19.py
+++ python/2023/day19.py
@@ -1,6 +1,8 @@
 import dataclasses
 import operator
 import re
+from collections import defaultdict, deque
+from copy import deepcopy
 
 from common.parsing import parse_numbers
 
@@ -123,9 +125,111 @@ def part1(data):
     return sum(part.get_value() for part in accepted_parts)
 
 
+class State:
+    def __init__(self, node):
+        self.accepted = None
+        self.conditions = defaultdict(list)
+        self.node = node
+
+    @classmethod
+    def copy(cls, state):
+        copied_state = cls(state.node)
+        copied_state.accepted = state.accepted
+        copied_state.conditions = deepcopy(state.conditions)
+
+        return copied_state
+
+    def __repr__(self):
+        return f"State ({self.node}) {self.conditions}"
+
+    def get_value(self):
+        total = 1
+        for condition_key in ("x", "m", "a", "s"):
+            accepted_values = set(range(4000))
+
+            for restriction in self.conditions[condition_key]:
+                accepted_values.intersection_update(restriction)
+
+            total *= len(accepted_values)
+
+        return total
+
+
+class WorkflowV2:
+    def __init__(self, rule_data):
+        self.rules = []
+
+        for data in rule_data.split(","):
+            match data.split(":"):
+                case [condition, result]:
+                    attribute, comparator, magnitude = CONDITON_RE.match(condition).groups()
+                    self.rules.append((attribute, comparator, int(magnitude), result))
+                case [result]:
+                    self.rules.append((True, result))
+
+    def process(self, state):
+        return_states = []
+        prev_state = state
+        for rule in self.rules:
+            match rule:
+                case attribute, comparator, magnitude, result:
+                    magnitude -= 1
+                    success_state = State.copy(prev_state)
+                    fail_state = State.copy(prev_state)
+                    if comparator == "<":
+                        condition = range(magnitude)
+                        condition_fail = range(magnitude, 4000)
+                    else:
+                        condition = range(magnitude + 1, 4000)
+                        condition_fail = range(magnitude + 1)
+                    success_state.conditions[attribute].append(condition)
+                    success_state.node = result
+                    fail_state.conditions[attribute].append(condition_fail)
+
+                    return_states.append(success_state)
+                    prev_state = fail_state
+                case _, result:
+                    prev_state.node = result
+                    return_states.append(prev_state)
+
+        return return_states
+
+
 def part2(data):
-    data = EXAMPLE_DATA
+    # data = EXAMPLE_DATA
 
+    WORKFLOWS = {}
 
+    data_gen = iter(data.splitlines())
 
-    return data
+    while True:
+        workflow_data = next(data_gen)
+
+        if not workflow_data:
+            break
+
+        rules_start = workflow_data.find("{")
+        name, rule_data = workflow_data[:rules_start], workflow_data[rules_start + 1:-1]
+
+        WORKFLOWS[name] = WorkflowV2(rule_data)
+
+    initial_state = State("in")
+    queue = deque()
+    queue.append(initial_state)
+
+    total = 0
+    while queue:
+        current_state = queue.popleft()
+        next_states = WORKFLOWS[current_state.node].process(current_state)
+
+        for state in next_states:
+            if state.node == "A":
+                total += state.get_value()
+                continue
+
+            if state.node == "R":
+                continue
+
+            queue.append(state)
+
+    return total
